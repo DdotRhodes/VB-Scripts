419d20b6ab96b62f85a20bd6c53d948bf257b0f0
DdotRhodes <d.rhodes@me.com>
2016-12-19 08:12:41 -0800

    Update UACFix.vb
    Puts in message box notifying an error will occur if not run as administrator.

--- UACFix.vb
+++ UACFix.vb
@@ -9,6 +9,8 @@ Option Explicit
 Dim objShell
 Dim strUAC, strNewEntry1, strNewEntry2, strSystemHost
 
+MsgBox "This application must be run as Administrator. If not run as Administrator, it will error out."
+
 'Registry input variables
 strUAC = "ConsentPromptBehaviorAdmin"
 
@@ -17,5 +19,5 @@ strSystemHost = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Po
 
 Set objShell = CreateObject("WScript.Shell")
 
-'Changing the registry inputs
+'Changing UAC to level 2
 strNewEntry1 = objShell.RegWrite(strSystemHost & strUAC, 5, "REG_DWORD")
